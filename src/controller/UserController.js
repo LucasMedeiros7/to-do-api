d110a34febecdc2dfae26610b7ce0968dd153264
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-13 16:36:35 -0300

    test deploy heroku

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -2,12 +2,16 @@ import { connection } from '../database/database.js';
 
 export default {
   async get(req, res) {
-    res.send('Hello World');
+    res.json('Rota funcionando');
   },
 
   async getUsers(req, res) {
     try {
       const { rows } = await connection.query('SELECT * FROM "USUARIOS"');
+
+      rows.forEach(row => {
+        row.SENHA = undefined;
+      });
 
       res.send(rows);
     } catch (error) {
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import UserController from './controller/UserController.js';
 const routes = Router();
 
 routes.get('/users', UserController.getUsers);
+routes.get('/', UserController.get);
 
 export { routes };
