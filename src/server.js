a2c79059837eb5ab0b6c6923a59d52ae3550dc30
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-13 13:35:02 -0300

    limapando corsConfig

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import chalk from 'chalk';
 import UsuariosController from './controllers/UsuariosController.js';
 import TarefasController from './controllers/TarefasController.js';
@@ -7,6 +8,7 @@ import bd from './infra/sqlite-db.js';
 const app = express();
 const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 
 UsuariosController(app, bd);
