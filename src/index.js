a5a3851497f74baeb1ab7955393d4efe7c50347e
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-15 19:32:02 -0300

    add dotenv index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { userRoutes } from './userRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3333;
