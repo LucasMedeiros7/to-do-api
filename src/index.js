46e897fd1116dd8e00f8ffe45b74561e050e61a6
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-15 12:40:43 -0300

    debug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { userRoutes } from './userRoutes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log('heroku', process.env.NODE_ENV);
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+console.log(port);
 
 app.use(cors());
 app.use(express.json());
